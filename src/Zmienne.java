711ad1bb5732224d2bed8b25100420535bdb3957
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-16 16:51:21 +0100

    zmienne_def

---
+++ src/Zmienne.java
@@ -0,0 +1,13 @@
+public class Zmienne {
+    public static void main(String[] args) {
+        int firstNumber, secondNumber, result;
+        String name = "Mateusz";
+        firstNumber  = 1;
+        secondNumber = 2;
+        result = firstNumber + secondNumber;
+        name = "Kasia";
+        System.out.println(name);
+        System.out.println(result);
+
+    }
+}
