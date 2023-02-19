0378e286a33c9e7ea8dc60bff8d8dedc9a62f15b
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-19 20:10:56 +0100

    Exercise1

---
+++ src/Exercise_calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise_calculator {
+    public static void main(String[] args) {
+        //first number
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Prosze podaj pierwsza liczbe");
+        int firstNumber = scanner1.nextInt();
+
+        //second number
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Prosze podaj druga liczbe");
+        int secondNumber = scanner2.nextInt();
+
+        //mathematical operations
+        int addition = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        int division = firstNumber / secondNumber;
+        int mod = secondNumber%firstNumber; //modulo to reszta z dzielenia
+
+        System.out.println("Wyniki dodawania "+ addition);
+        System.out.println("Wyniki odejmowania "+ subtraction);
+        System.out.println("Wyniki mnozenia "+ multiplication);
+        System.out.println("Wyniki dzielenia "+ division);
+        System.out.println("Wyniki modulo "+ mod);
+    }
+}
