5bd7da3a1f99ecdc5b526446bb63856e516811b3
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-19 19:49:21 +0100

    Scanner

---
+++ src/CustomScanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CustomScanner {
+/*    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Prosze podaj swoje imie");
+        String name = scanner.nextLine();
+        System.out.println("Czesc " + name + "!");*/
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Prosze podaj pierwsza liczbe");
+            int firstNumber = scanner.nextInt();
+            int result = firstNumber * firstNumber;
+            System.out.println("Kwadrat tej liczby to " + result);
+    }
+}
