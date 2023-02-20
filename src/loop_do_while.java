04bf9514ac273b930e2dd2b88de82d65a0bd3126
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-20 17:14:10 +0100

    petla while do

---
+++ src/loop_do_while.java
@@ -0,0 +1,13 @@
+public class loop_do_while {
+    public static void main(String[] args) {
+
+        int number = 21;
+
+         do {
+            System.out.println("Moja wartosc to: " + number + " i jestem mniejszy od 20");
+            number++;
+        }
+
+        while (number < 20);
+    }
+}
