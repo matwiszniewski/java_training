ec621342ca4ba19ff5824285b1e540935c48e827
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-20 13:51:50 +0100

    conditional statement if

---
+++ src/conditional_statement.java
@@ -0,0 +1,11 @@
+public class conditional_statement {
+    public static void main(String[] args) {
+        float a = 10;
+        float b = 0;
+
+        if (b!=0) {
+            System.out.println("Wynik dzielenia to: " + (a/b));
+        }
+        else {System.out.println("Prosze podaj inna liczbe");}
+    }
+}
