1c029f14f5940551af14bd125a921b1aaef1c533
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-20 10:03:29 +0100

    Comparison operators

---
+++ src/Comparison_operators.java
@@ -0,0 +1,20 @@
+public class Comparison_operators {
+    public static void main(String[] args) {
+        int firstNumber = 4;
+        int secondNumber = 6;
+
+        boolean result = firstNumber > secondNumber;
+
+        System.out.println(result);
+
+        System.out.println(firstNumber > secondNumber);
+        System.out.println(firstNumber < secondNumber);
+        System.out.println(firstNumber >= secondNumber);
+        System.out.println(firstNumber <= secondNumber);
+        System.out.println(firstNumber > secondNumber);
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber != secondNumber);
+
+
+    }
+}
