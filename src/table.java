08ed5caf0bdb8586ab64a1f15a096b4cd4f5870f
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-21 22:19:38 +0100

    tablice

---
+++ src/table.java
@@ -0,0 +1,50 @@
+public class table {
+/*    public static void main(String[] args) {
+
+        String [] winterMonths = new String[3];
+            winterMonths[0] = "grudzien";
+            winterMonths[1] = "styczen";
+            winterMonths[2] = "luty";
+
+
+        int[] numbers = new int[] {1,4,6};
+
+        System.out.println(winterMonths[0]);
+        System.out.println(winterMonths[1]);
+        System.out.println(winterMonths[2]);
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+        System.out.println(winterMonths.length);
+
+    }*/
+
+    public static void main(String[] args) {
+
+        String [] imiona = new String[3];
+            imiona [0] = "Mateusz";
+            imiona [1] = "Daniel";
+            imiona [2] = "Basia";
+
+        System.out.println(imiona[0]);
+        System.out.println(imiona[1]);
+        System.out.println(imiona[2]);
+
+        int [] lottonNumbers = new int[] {1,2,3,4,5,6};
+
+  /*      System.out.println(lottonNumbers[0]);
+        System.out.println(lottonNumbers[1]);
+        System.out.println(lottonNumbers[2]);
+        System.out.println(lottonNumbers[3]);
+        System.out.println(lottonNumbers[4]);
+        System.out.println(lottonNumbers[5]);
+
+        System.out.println(lottonNumbers.length);*/
+
+        for (int i = 0; i < lottonNumbers.length; i++) {
+            System.out.println(lottonNumbers[i]);
+        }
+    }
+}
