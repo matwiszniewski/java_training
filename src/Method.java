4eb562eeee1ecf319db8e3c4e3ccb5002b001e78
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-22 16:50:49 +0100

    Methods

---
+++ src/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+
+    public void policzWynik () {
+        System.out.println("Zaraz policze wynik");
+        int result = 0;
+        for (int i=0; i<100; i++) {
+            result = result +1;
+        }
+        System.out.println("Rezultat to " + result);
+    }
+}
---
+++ src/Method_test.java
@@ -0,0 +1,12 @@
+public class Method_test {
+
+    public static void main(String[] args) {
+
+        Method metoda1 = new Method();
+        metoda1.policzWynik();
+        metoda1.policzWynik();
+        metoda1.policzWynik();
+
+
+    }
+}
