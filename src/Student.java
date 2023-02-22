d4ca53fd81cfa10204fb7e0c88a0db1e25932d7c
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-22 15:05:01 +0100

    klasy

---
+++ src/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+
+    public String imie;
+    public String nazwisko;
+    public String nick;
+    public String email;
+    public int numerIndeksu;
+
+    public void przedstawSie () {
+        System.out.println("Nazywam sie " + imie+" "+nazwisko);
+    }
+    public void zalogujSie () {
+        System.out.println("Loguje sie za pomoca " + nick);
+    }
+
+    public void podajNrIndeksu () {
+        System.out.println("Moj numer indeksu to " +numerIndeksu);
+    }
+    public void podajEmail () {
+        System.out.println("Moj numer email to " +email);
+    }
+
+
+
+}
