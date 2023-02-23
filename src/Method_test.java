880f4b33fe83b2bfdc38affeba4cf2dbfb2cff0b
matwiszniewski <mat.wiszniewski@gmail.com>
2023-02-23 12:11:50 +0100

    Methods2

--- src/Method_test.java
+++ src/Method_test.java
@@ -4,8 +4,25 @@ public class Method_test {
 
         Method metoda1 = new Method();
         metoda1.policzWynik();
-        metoda1.policzWynik();
-        metoda1.policzWynik();
+
+
+
+        int result = metoda1.pobierzWynik();
+        int result2 = result * 2;
+        System.out.println("Rezultat przed mnozeniem to "+result);
+        System.out.println("Rezultat po mnozeniu to "+result2);
+
+
+        metoda1.policzWynikParam(5);
+        metoda1.policzWynikParam(100);
+        metoda1.policzWynikParam(23);
+
+
+        int result3 = metoda1.add(2,3);
+        System.out.println(result3);
+        System.out.println(metoda1.add(0,5));
+
+
 
 
     }
