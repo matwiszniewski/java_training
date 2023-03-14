b80df9263fd4e8f4433d9de43f64723ea87572ec
matwiszniewski <mat.wiszniewski@gmail.com>
2023-03-14 21:55:53 +0100

    method overriding

--- src/Footballer.java
+++ src/Footballer.java
@@ -8,6 +8,9 @@ public class Footballer extends Person {
         this.footballClub = footballClub;
     }
 
+    public void eat () {
+        System.out.println("I like healthy food");
+    }
     public void playFootball () {
         System.out.println("I play football for " + footballClub);
     }
