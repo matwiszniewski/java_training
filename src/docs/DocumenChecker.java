9797a8237eb8c856f3535325848a4301348978c3
matwiszniewski <mat.wiszniewski@gmail.com>
2023-03-22 10:20:35 +0100

    polimorfizm2

---
+++ src/docs/DocumenChecker.java
@@ -0,0 +1,8 @@
+package docs;
+
+public class DocumenChecker {
+    public static void main(String[] args) {
+        Document doc = new PdfDocument();
+        doc.getDescription();
+    }
+}
---
+++ src/docs/ExecelDocument.java
@@ -0,0 +1,9 @@
+package docs;
+
+public class ExecelDocument implements Document {
+    @Override
+    public void getDescription() {
+        System.out.println("I am excel document");
+
+    }
+}
