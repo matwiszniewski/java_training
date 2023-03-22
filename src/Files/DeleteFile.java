d8f9bd450f9bb00ab109923d11296ed26bfc17d2
matwiszniewski <mat.wiszniewski@gmail.com>
2023-03-22 17:49:17 +0100

    add write read delete file

---
+++ src/Files/DeleteFile.java
@@ -0,0 +1,15 @@
+package Files;
+
+import java.io.File;
+
+public class DeleteFile {
+    public static void main(String[] args) {
+
+        File file = new File("test.txt");
+        if (file.delete()) {
+            System.out.println("Usunelismy plik");
+        } else {
+            System.out.println("Nie udalo sie");
+        }
+    }
+}
---
+++ src/Files/WriteToFile.java
@@ -0,0 +1,13 @@
+package Files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter writer = new FileWriter("test.txt");
+        writer.write("New text");
+        writer.close();
+    }
+}
